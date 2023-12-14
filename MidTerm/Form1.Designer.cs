57c5271b0d26f42cab8e9adb08119edc656886a9
Julimorg <kienphongtran2003@gmail.com>
2023-12-14 13:28:45 +0700

    midterm

---
+++ MidTerm/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace MidTerm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sideBar = new Guna.UI2.WinForms.Guna2Panel();
+            this.MenuDropDown = new System.Windows.Forms.FlowLayoutPanel();
+            this.ButtonSelect = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button4 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button5 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button6 = new Guna.UI2.WinForms.Guna2Button();
+            this.panelchild = new Guna.UI2.WinForms.Guna2Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.sideBar.SuspendLayout();
+            this.MenuDropDown.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sideBar
+            // 
+            this.sideBar.BackColor = System.Drawing.Color.LightGray;
+            this.sideBar.Controls.Add(this.MenuDropDown);
+            this.sideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sideBar.Location = new System.Drawing.Point(0, 0);
+            this.sideBar.Name = "sideBar";
+            this.sideBar.Size = new System.Drawing.Size(300, 961);
+            this.sideBar.TabIndex = 0;
+            // 
+            // MenuDropDown
+            // 
+            this.MenuDropDown.Controls.Add(this.ButtonSelect);
+            this.MenuDropDown.Controls.Add(this.guna2Button1);
+            this.MenuDropDown.Controls.Add(this.guna2Button2);
+            this.MenuDropDown.Controls.Add(this.guna2Button3);
+            this.MenuDropDown.Controls.Add(this.guna2Button4);
+            this.MenuDropDown.Controls.Add(this.guna2Button5);
+            this.MenuDropDown.Controls.Add(this.guna2Button6);
+            this.MenuDropDown.Location = new System.Drawing.Point(0, 0);
+            this.MenuDropDown.Name = "MenuDropDown";
+            this.MenuDropDown.Size = new System.Drawing.Size(300, 80);
+            this.MenuDropDown.TabIndex = 0;
+            // 
+            // ButtonSelect
+            // 
+            this.ButtonSelect.Animated = true;
+            this.ButtonSelect.BackColor = System.Drawing.Color.Transparent;
+            this.ButtonSelect.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.ButtonSelect.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.ButtonSelect.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.ButtonSelect.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.ButtonSelect.FillColor = System.Drawing.Color.LightSlateGray;
+            this.ButtonSelect.Font = new System.Drawing.Font("Nunito SemiBold", 12F, System.Drawing.FontStyle.Bold);
+            this.ButtonSelect.ForeColor = System.Drawing.Color.Black;
+            this.ButtonSelect.Location = new System.Drawing.Point(3, 3);
+            this.ButtonSelect.Name = "ButtonSelect";
+            this.ButtonSelect.Size = new System.Drawing.Size(297, 76);
+            this.ButtonSelect.TabIndex = 2;
+            this.ButtonSelect.Text = "Button";
+            this.ButtonSelect.UseTransparentBackground = true;
+            this.ButtonSelect.Click += new System.EventHandler(this.ButtonSelect_Click);
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.Animated = true;
+            this.guna2Button1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.LightSlateGray;
+            this.guna2Button1.Font = new System.Drawing.Font("Nunito SemiBold", 12F, System.Drawing.FontStyle.Bold);
+            this.guna2Button1.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button1.Location = new System.Drawing.Point(3, 85);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(297, 76);
+            this.guna2Button1.TabIndex = 3;
+            this.guna2Button1.Text = "Button";
+            this.guna2Button1.UseTransparentBackground = true;
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.Animated = true;
+            this.guna2Button2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.LightSlateGray;
+            this.guna2Button2.Font = new System.Drawing.Font("Nunito SemiBold", 12F, System.Drawing.FontStyle.Bold);
+            this.guna2Button2.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button2.Location = new System.Drawing.Point(3, 167);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(297, 76);
+            this.guna2Button2.TabIndex = 4;
+            this.guna2Button2.Text = "Button";
+            this.guna2Button2.UseTransparentBackground = true;
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.Animated = true;
+            this.guna2Button3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button3.FillColor = System.Drawing.Color.LightSlateGray;
+            this.guna2Button3.Font = new System.Drawing.Font("Nunito SemiBold", 12F, System.Drawing.FontStyle.Bold);
+            this.guna2Button3.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button3.Location = new System.Drawing.Point(3, 249);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.Size = new System.Drawing.Size(297, 76);
+            this.guna2Button3.TabIndex = 5;
+            this.guna2Button3.Text = "Button";
+            this.guna2Button3.UseTransparentBackground = true;
+            // 
+            // guna2Button4
+            // 
+            this.guna2Button4.Animated = true;
+            this.guna2Button4.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button4.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button4.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button4.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button4.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button4.FillColor = System.Drawing.Color.LightSlateGray;
+            this.guna2Button4.Font = new System.Drawing.Font("Nunito SemiBold", 12F, System.Drawing.FontStyle.Bold);
+            this.guna2Button4.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button4.Location = new System.Drawing.Point(3, 331);
+            this.guna2Button4.Name = "guna2Button4";
+            this.guna2Button4.Size = new System.Drawing.Size(297, 76);
+            this.guna2Button4.TabIndex = 6;
+            this.guna2Button4.Text = "Button";
+            this.guna2Button4.UseTransparentBackground = true;
+            // 
+            // guna2Button5
+            // 
+            this.guna2Button5.Animated = true;
+            this.guna2Button5.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button5.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button5.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button5.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button5.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button5.FillColor = System.Drawing.Color.LightSlateGray;
+            this.guna2Button5.Font = new System.Drawing.Font("Nunito SemiBold", 12F, System.Drawing.FontStyle.Bold);
+            this.guna2Button5.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button5.Location = new System.Drawing.Point(3, 413);
+            this.guna2Button5.Name = "guna2Button5";
+            this.guna2Button5.Size = new System.Drawing.Size(297, 76);
+            this.guna2Button5.TabIndex = 7;
+            this.guna2Button5.Text = "Button";
+            this.guna2Button5.UseTransparentBackground = true;
+            // 
+            // guna2Button6
+            // 
+            this.guna2Button6.Animated = true;
+            this.guna2Button6.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button6.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button6.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button6.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button6.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button6.FillColor = System.Drawing.Color.LightSlateGray;
+            this.guna2Button6.Font = new System.Drawing.Font("Nunito SemiBold", 12F, System.Drawing.FontStyle.Bold);
+            this.guna2Button6.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button6.Location = new System.Drawing.Point(3, 495);
+            this.guna2Button6.Name = "guna2Button6";
+            this.guna2Button6.Size = new System.Drawing.Size(297, 76);
+            this.guna2Button6.TabIndex = 8;
+            this.guna2Button6.Text = "Button";
+            this.guna2Button6.UseTransparentBackground = true;
+            // 
+            // panelchild
+            // 
+            this.panelchild.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelchild.Location = new System.Drawing.Point(300, 0);
+            this.panelchild.Name = "panelchild";
+            this.panelchild.Size = new System.Drawing.Size(1484, 961);
+            this.panelchild.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1784, 961);
+            this.Controls.Add(this.panelchild);
+            this.Controls.Add(this.sideBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.sideBar.ResumeLayout(false);
+            this.MenuDropDown.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel sideBar;
+        private Guna.UI2.WinForms.Guna2Panel panelchild;
+        private System.Windows.Forms.FlowLayoutPanel MenuDropDown;
+        private System.Windows.Forms.Timer timer1;
+        private Guna.UI2.WinForms.Guna2Button ButtonSelect;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button4;
+        private Guna.UI2.WinForms.Guna2Button guna2Button5;
+        private Guna.UI2.WinForms.Guna2Button guna2Button6;
+    }
+}
+
---
+++ MidTerm/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MidTerm
+{
+    public partial class Form1 : Form
+    {
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool isExpand = false;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(isExpand == false)
+            {
+                MenuDropDown.Height += 1000;
+
+                if (MenuDropDown.Height >= MenuDropDown.MaximumSize.Height) 
+                {
+                    timer1.Stop();
+                    isExpand = true;
+                   
+                }
+            }
+           else
+            {
+                MenuDropDown.Height -= 1000;
+                if(MenuDropDown.Height >= MenuDropDown.MinimumSize.Height)
+                {
+                     
+                    timer1.Stop();
+                    isExpand = false; 
+                }
+            }
+
+        }
+    
+        private void ButtonSelect_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+       
+    }
+}
